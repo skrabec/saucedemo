c5bd539ef2d2ace092f71c4ed3058be24b446eee
unknown <skrabets@gmail.com>
2022-12-16 22:39:14 +0100

    Edit pom and LoginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,2 +1,38 @@
-package pages;public class LoginPage {
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+
+    @FindBy(xpath = "//input[@placeholder='Username']")
+    private WebElement usernameTextBox;
+
+    @FindBy(xpath = "//input[@placeholder='Password']")
+    private WebElement passwordTextBox;
+
+    @FindBy(xpath = "//input[@value='LOGIN']")
+    private WebElement loginButton;
+
+    public WebDriver driver;
+    public static String url = "https://www.saucedemo.com/";
+
+    public static LoginPage visitPage(WebDriver driver) {
+        LoginPage page = new LoginPage(driver);
+        page.visitPage();
+        return page;
+    }
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void visitPage() {
+        this.driver.get(url);
+    }
 }
